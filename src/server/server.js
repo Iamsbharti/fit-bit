05a386d88351013446661f328813cbc3c956ab4c
Saurabh <saurabhbharti9@gmail.com>
2020-05-31 00:07:03 +0530

    initiatized server

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+
+//initialize a port
+let port = process.env.PORT || "4200";
+
+//initialize the express app
+let app = express();
+
+//listen to requests
+app.listen(port, console.log(`API Server Started at -${port}`));
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("Get works");
+});
