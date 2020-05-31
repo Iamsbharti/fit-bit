9675f7c3c9436feb421940737a370eb9f93664a9
Saurabh <saurabhbharti9@gmail.com>
2020-05-31 22:18:32 +0530

    added Inputform component

---
+++ src/components/InputForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { DialogActions, Dialog } from "@material-ui/core";
+export default function () {
+  return (
+    <div>
+      <Dialog>
+        <DialogActions></DialogActions>
+      </Dialog>
+    </div>
+  );
+}
