bd81477c9ece100fae0e076895e1c30fccc8fdcf
Saurabh <saurabhbharti9@gmail.com>
2020-05-31 22:37:39 +0530

    configured store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import configureStore from "./redux/store";
+import { Provider as ReduxProvider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//initialize store
+const store = configureStore();
+
+ReactDOM.render(
+  <ReduxProvider store={store}>
+    <App />
+  </ReduxProvider>,
+  document.getElementById("root")
+);
